ee321293e3d105a5f966335af4eb3f16af34018f
WolvenAnthros <mas_135@mail.ru>
2022-03-30 16:22:06 +0300

    Everything works except for time step(tau).

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ I = np.array(
 )
 # initial parameters
 omega_01 = 5
-omega_R = 5.4
-amplitude_R = 2
-time = 1.5
-counts = 111
+omega_R = 5
+amplitude_R = 0.1
+time = 100
+counts = 10000
 tau = time / counts
 # initial psi (determines the initial state)
 psi = np.array(
@@ -30,9 +30,9 @@ psi = np.array(
 
 end_probability_excited = []
 end_probability_ground = []
-
+end_Rhabi = []
 for k in range(counts):
-    oscillatory_part = amplitude_R * np.cos(omega_R * (k * tau + tau / 2))
+    oscillatory_part = amplitude_R * np.cos(omega_R * (2*k * tau + tau / 2))
     # initial excited/ground state
     excited_state = np.array(
         [1, 0]
@@ -58,6 +58,10 @@ for k in range(counts):
     psi = np.matmul(fraction, psi)
     # norm of the psi matrix
     norm = abs(np.matmul(psi, psi.conjugate()))
+    # Rhabi solution for comparison
+    Omega = np.sqrt((omega_R - omega_01) ** 2 + amplitude_R ** 2)
+    Rhabi = 1 - amplitude_R ** 2 / Omega**2 * np.sin(Omega * k*tau / 2) ** 2
+    end_Rhabi.append(Rhabi)
     print(norm)
 
 # plot image
@@ -65,6 +69,7 @@ axis = [x for x in range(counts)]
 fig, at = plt.subplots()
 at.plot(axis, end_probability_excited, label='excited state')
 at.plot(axis, end_probability_ground, label='ground state')
+at.plot(axis, end_Rhabi, label='Rhabi solution')
 at.set_xlabel('time')
 at.set_ylabel('probability')
 at.set_title("States graph")
