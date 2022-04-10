df59dc594bfb8ce378df4d77ba4cd5aad8d69785
WolvenAnthros <mas_135@mail.ru>
2022-04-10 22:23:32 +0300

    Serious discrepancy with Rhabi's solution due to sigma_y in Hamiltonian

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ sigma_x = np.array(
     [[0, 1],
      [1, 0]]
 )
+sigma_y = np.array(
+    [[0, -1j],
+     [1j, 0]]
+)
 # identity matrix
 I = np.array(
     [[1, 0],
@@ -25,6 +29,10 @@ amplitude_R = np.pi
 time = 100
 counts = 2600
 tau = time / counts
+alpha = 1
+beta = 0.5
+sigma = 3
+t_g = 4 * sigma
 # initial psi (determines the initial state)
 psi = np.array(
     [1, 0]
@@ -33,17 +41,8 @@ psi = np.array(
 end_probability_excited = []
 end_probability_ground = []
 end_Rhabi = []
+
 for k in range(counts):
-    t_k = lambda k, tau: k * tau
-    alpha = 1
-    sigma = 3
-    t_g = 4 * sigma
-    Omega_x_numerator = np.exp(-(t_k(k, tau) - 0.5 + t_g) ** 2 / (2 * sigma ** 2)) - np.exp(
-        -t_g ** 2 / (8 * sigma ** 2))
-    Omega_x_denominatior = np.sqrt(2 * np.pi * sigma ** 2) * math.erf(t_g / (np.sqrt(8) * sigma)) - t_g * np.exp(
-        -t_g ** 2 / (8 * sigma ** 2))
-    Omega_x = alpha * amplitude_R * (Omega_x_numerator) / Omega_x_denominatior
-    oscillatory_part = Omega_x * np.cos(omega_R * (2 * t_k(k + 1 / 2, tau)))
     # initial excited/ground state
     excited_state = np.array(
         [1, 0]
@@ -51,7 +50,6 @@ for k in range(counts):
     ground_state = np.array(
         [0, 1]
     )
-
     # excited probability formula
     probability_excited = np.matmul(excited_state, psi)
     probability_excited = abs(probability_excited * probability_excited.conjugate())
@@ -60,23 +58,50 @@ for k in range(counts):
     probability_ground = np.matmul(ground_state, psi)
     probability_ground = abs(probability_ground * probability_ground.conjugate())
     end_probability_ground.append(probability_ground)
+    # Start of psi calculation
+    t = lambda k_, tau_: k_ * tau_
+    # Oscillatory amplitude (related to DRAG techinque)
+    Omega_x_numerator = np.exp(-(t(k, tau) - 0.5 + t_g) ** 2 / (2 * sigma ** 2)) - np.exp(
+        -t_g ** 2 / (8 * sigma ** 2))
+    Omega_x_denominatior = np.sqrt(2 * np.pi * sigma ** 2) * math.erf(t_g / (np.sqrt(8) * sigma)) - t_g * np.exp(
+        -t_g ** 2 / (8 * sigma ** 2))
+    Omega_x = alpha * amplitude_R * Omega_x_numerator / Omega_x_denominatior
+
+    Omega_y_numerator = np.exp(-(t(k, tau) - 0.5 + t_g) ** 2 / (2 * sigma ** 2)) * (t(k, tau) - 0.5 * t_g)
+    Omega_y_denominator = (sigma ** 2) * Omega_x_denominatior
+    Omega_y = -beta * amplitude_R * (-Omega_y_numerator / Omega_y_denominator)
+
+    oscillatory_part = Omega_x * np.cos(omega_R * (2 * t(k + 1 / 2, tau))) + Omega_y * np.sin(
+        omega_R * (2 * t(k + 1 / 2, tau)))
+    oscillatory_part_1st_derivative = -Omega_x * omega_R * np.sin(
+        omega_R * (t(k + 1 / 2, tau))) + Omega_y * omega_R * np.cos(omega_R * (2 * t(k + 1 / 2, tau)))
+    oscillatory_part_2nd_derivative = -Omega_x * omega_R ** 2 * np.cos(
+        omega_R * (t(k + 1 / 2, tau))) - Omega_y * omega_R ** 2 * np.sin(omega_R * (2 * t(k + 1 / 2, tau)))
+
     # Crank-Nicolson 2nd order method implementation
-    Hamiltonian = omega_01 * sigma_z + oscillatory_part * sigma_x
-    Hamiltonian_1st_derivative = -Omega_x * omega_R * np.sin(omega_R * (t_k(k + 1 / 2, tau)))
-    Hamiltonian_2nd_derivative = -Omega_x * (omega_R ** 2) * np.cos(omega_R * (t_k(k + 1 / 2, tau)))
+    # ////// Should these matrices (sigma_x, sigma_y) be applied to Hamiltonian in the new theory? ///////
+    Hamiltonian = omega_01 * sigma_z + oscillatory_part # * (sigma_x + sigma_y)
+    Hamiltonian_1st_derivative = oscillatory_part_1st_derivative # * (sigma_x + sigma_y)
+    Hamiltonian_2nd_derivative = oscillatory_part_2nd_derivative # * (sigma_x + sigma_y)
     commutator = Hamiltonian_1st_derivative * Hamiltonian - Hamiltonian * Hamiltonian_1st_derivative
+
     F = Hamiltonian + (tau ** 2 / 24) * Hamiltonian_2nd_derivative - 1j * (tau ** 2 / 12) * commutator
+
     numerator = I - (tau ** 2 / 12) * np.matmul(F, F) - 1j * (tau / 2) * F
     denominator = I - (tau ** 2 / 12) * np.matmul(F, F) + 1j * (tau / 2) * F
     denominator = np.linalg.inv(denominator)
     fraction = np.matmul(denominator, numerator)
+
     psi = np.matmul(fraction, psi)
+
     # norm of the psi matrix
     norm = abs(np.matmul(psi, psi.conjugate()))
+
     # Rhabi soultion for comparison
-    Omega = np.sqrt((omega_R - omega_01) ** 2 + Omega_x ** 2)
-    Rhabi = 1 - Omega_x ** 2 / Omega ** 2 * np.sin(Omega * t_k(k, tau) / 2) ** 2
+    Omega = np.sqrt((omega_R - omega_01) ** 2 + (Omega_x + Omega_y) ** 2)
+    Rhabi = 1 - (Omega_x + Omega_y) ** 2 / Omega ** 2 * np.sin(Omega * t(k, tau) / 2) ** 2
     end_Rhabi.append(Rhabi)
+
     print(norm)
 
 # plot image
